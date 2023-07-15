a75bf21a7100cd6e2a1472e9b01b96438eb467f5
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-15 21:55:25 +0700

    Initial commit

---
+++ src/main/java/com/zirui/tutorialmod/TutorialModClient.java
@@ -0,0 +1,11 @@
+package com.zirui.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
